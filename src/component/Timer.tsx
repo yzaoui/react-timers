88b2491b46c1b37648f4ee4832a24f99c1a67f00
Yann Zaoui <yzaoui7@gmail.com>
2019-08-08 00:03:19 -0400

    Convert time display to human-friendly format

--- src/component/Timer.tsx
+++ src/component/Timer.tsx
@@ -13,7 +13,7 @@ const Timer: React.FC<TimerProps> = ({ title, project, elapsedms }) => {
                 <div className="header">{title}</div>
                 <div className="meta">{project}</div>
                 <div className="center aligned description">
-                    <h2>{elapsedms}</h2>
+                    <h2>{millisecondsToHourMinuteSecond(elapsedms)}</h2>
                 </div>
                 <div className="extra content">
                     <span className="right floated edit icon">
@@ -29,6 +29,16 @@ const Timer: React.FC<TimerProps> = ({ title, project, elapsedms }) => {
             </div>
         </div>
     )
+};
+
+const millisecondsToHourMinuteSecond = (ms: number): string => {
+    const hours = Math.floor(ms / 1000 / 60 / 60);
+    const minutes = Math.floor((ms / 1000 / 60) % 60);
+    const seconds = Math.floor((ms / 1000) % 60);
+
+    return [hours, minutes, seconds]
+        .map((num) => num < 10 ? `0${num}` : `${num}`)
+        .join(":");
 };
 
 export default Timer;
--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import Timer from './component/Timer';
 import * as serviceWorker from './serviceWorker';
 import "semantic-ui-css/semantic.min.css";
 
-ReactDOM.render(<Timer title="First Timer" project="Project" elapsedms={1234}/>, document.getElementById('root'));
+ReactDOM.render(<Timer title="First Timer" project="Project" elapsedms={12345678}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
